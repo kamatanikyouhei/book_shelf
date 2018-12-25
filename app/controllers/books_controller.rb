a76edca509f93333f17786d808cfb9653a51a2e9
kamatanikyouhei <kamatanikyouhei@kamatanikyouheinoMacBook-ea.local>
2018-12-25 13:16:13 +0900

    バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,11 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to @book,notice:"書籍を登録いたしました"
+    if  @book.save
+      redirect_to @book,notice:"書籍を登録いたしました"
+    else
+      render :new 
+    end
   end
 
   def show
